e1d74ac59056ae4dbf326afee6591e1f02c8c3e2
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2019-02-24 18:00:25 +0100

    Update CreatVideoFromPic.m

--- MATLAB/Video/CreatVideoFromPic.m
+++ MATLAB/Video/CreatVideoFromPic.m
@@ -29,8 +29,40 @@ function CreatVideoFromPic(dn, picformat,aviname)
     end
     close(aviobj);
 end
---------------------- 
-作者：Strangers_bye 
-来源：CSDN 
-原文：https://blog.csdn.net/u012526003/article/details/52610935 
-版权声明：本文为博主原创文章，转载请附上博文链接！
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+file_path = 'F:\数据\cqq\融合\11-08tig钛合金\16-47\低曝光';
+
+img_path_list = dir(strcat(file_path,'\*.bmp'));
+img_num = length(img_path_list);
+
+% path_in='Q:\08-21\11-11\';
+% path_in2='C:\Users\Administrator\Desktop\新建文件夹 2\';
+
+
+% temp1='C:\Users\Administrator\Desktop\视频变单帧图像\视频变单帧图像\bd\';% 路径名1
+WriterObj=VideoWriter('tig1647低曝光.mp4');% xxx.avi表示待合成的视频（不仅限于avi格式）的文件路径
+% WriterObj2=VideoWriter('2','MPEG-4');% xxx.avi表示待合成的视频（不仅限于avi格式）的文件路径
+open(WriterObj);
+% for i=1:img_num % n_frames表示图像帧的总数
+for i=1:200
+frame=imread(strcat(file_path,'\',img_path_list(i).name));% 读取图像，放在变量frame中
+           %  image = double(frame);
+            %b=frame(211:470,555:854,:);
+          %  image = image /65536 *256;
+           %  image = uint8(image);
+writeVideo(WriterObj,frame);% 将frame放到变量WriterObj中
+end
+close(WriterObj);
+
+% open(WriterObj2);
+% for i=1:325 % n_frames表示图像帧的总数
+% frame2=imread(strcat(path_in2,num2str(i)),'png');% 读取图像，放在变量frame中
+% writeVideo(WriterObj2,frame2);% 将frame放到变量WriterObj中
+% end
+% close(WriterObj2);
+
+
+
+
